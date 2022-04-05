b188c0e9ad8ddc1661af7699aee0e0782ec70dc6
Alexander Green <alexander.green@logius.nl>
2022-04-05 16:56:07 +0200

    release

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var respecConfig = {
   //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
-  specStatus: "VV",
+  specStatus: "DEF",
   //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
   specType: "ST",
   
@@ -10,10 +10,9 @@ var respecConfig = {
   // A YYYY-MM-DD date. The publication date of the present document. 
   // Als er geen publishDate is opgegeven, genereert ReSpec de dataum o.b.v. de laatste wijzigingen
   // Belangrijk: als publishDate niet opgegeven is, wordt bij de link "Laatst gepubliceerde versie" "geen" gezet. Anders wordt een link opgebouwd voor de laatste versie, met het formaat:
-  publishDate: "2021-03-24",
+  publishDate: "2022-04-11",
 
-  publishVersion: "1.0",
-  previousPublishVersion: "1.0-candidate", 
+  publishVersion: "1.0.0",
 
   addSectionLinks: true,
 
@@ -50,15 +49,6 @@ var respecConfig = {
   // this configuration can be set to make sure that the alle H1 headers and navigation bars are preserved
   nl_markdownSplitH1sections: true,
 
-  // in some cases the Repository of a Specification document and the Issue- and PR bases are split
-  // thi scan be configured below
-  // if not set, the url's will be generated on the respecConfig.github automatically  
-  nl_github: {
-    //  issueBase: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API/issues",
-    revision: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API",
-    //  pullrequests: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API/pulls"
-  },
-
   // nl_organisationName is used for some company specific values in the header (and Sotd)
   // currently supported: Logius and Geonovum (default)  
   nl_organisationName: "Logius",
@@ -66,11 +56,6 @@ var respecConfig = {
   // prefix for the names of company specific css, svg and ico prefixes
   // defaults to "GN-"  
   nl_organisationPrefix: "LS-",
-
-  // class style can be automatically insertd in generated markdown tables
-  // currently defaults to simple, but this may change
-  // options simple, data, <your css>    
-  nl_markdownTableClass: "dkkvs",
 
   // if nl_markdownEmbedImageInFigure is set to true images in markdown generated content will be surrounded with <figures> element
   // so that figures can be linked are be a part of table of figures
