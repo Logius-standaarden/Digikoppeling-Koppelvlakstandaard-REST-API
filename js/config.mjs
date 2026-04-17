2bf7d1361d440791e5b784e5ca5f828188dff56f
Alexander Green <alexander.green@logius.nl>
2026-04-17 14:39:11 +0200

    Trailing slashes mogen niet meer

--- js/config.mjs
+++ js/config.mjs
@@ -5,18 +5,18 @@ loadRespecWithConfiguration({
   authors: [ 
       { 
         "company" : "Logius",
-        "companyURL" : "https://www.logius.nl/",
+        "companyURL" : "https://www.logius.nl",
         "name" : "Peter Haasnoot"
       },
       { 
         "company" : "Logius",
-        "companyURL" : "https://www.logius.nl/",
+        "companyURL" : "https://www.logius.nl",
         "name" : "Pieter Hering"
       }
     ],
   editors: [ {    
         "company" : "Logius",  
-        "companyURL" : "https://www.logius.nl/",
+        "companyURL" : "https://www.logius.nl",
         "name" : "Standaardenbeheer"
       } ],
   github: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API",
